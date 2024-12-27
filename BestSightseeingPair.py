eda5b27bc9ead1d57e1b2946ae31b05ce1b327a2
Minokinad <danik.ivacewil@bk.ru>
2024-12-27 11:40:06 +0300

    1014. Best Sightseeing Pair Optimization

--- BestSightseeingPair.py
+++ BestSightseeingPair.py
@@ -3,14 +3,8 @@ from typing import List
 
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        pref = [0] * len(values)
-        suf = [0] * len(values)
-        pref[0] = values[0]
-        suf[-1] = values[-1] - len(values) + 1
-        for i in range(1, len(values)):
-            pref[i] = max(values[i] + i, pref[i - 1])
-            suf[-i - 1] = max(values[-i - 1] - (len(values) - i - 1), suf[-i])
-        ans = 0
-        for i in range(len(values) - 2, -1, -1):
-            ans = max(pref[i] + suf[i + 1], ans)
+        ans, dp = 0, 0
+        for i, j in enumerate(values):
+            ans = max(ans, dp + j - i)
+            dp = max(dp, j + i)
         return ans
